f2e2d2b8606f1db694b66fc760c8675905341a82
leonardojpeg <citizenleonardo@gmail.com>
2025-04-17 08:03:09 +0200

    correción estilo customerPanel (centrar título y botones, agregar color para igualar adminPanel)

--- app/views/CustomerView.php
+++ app/views/CustomerView.php
@@ -26,15 +26,39 @@ if (isset($_SESSION['flash_edit_message'])) {
 ?>
 
 <div class="container py-3 mt-5">
-    <h1 class="pb-5">Panel de administración de clientes</h1>
-    <div class="row justify-content-end">
+    <h1 class="pb-5 row justify-content-center">Panel de administración de clientes</h1>
+    <div class="row justify-content-center">
         <div class="col-auto">
+
+        <button type="button" class="btn text-white fw-bold" style="background-color: #007bff;"
+    onmouseover="this.style.backgroundColor='#0056b3'" 
+    onmouseout="this.style.backgroundColor='#007bff'" 
+    data-bs-toggle="modal" data-bs-target="#oneWayModal">
+    <i class="fa-solid fa-circle-plus"></i> Reservar Aeropuerto-Hotel
+</button>
+
+<button type="button" class="btn text-white fw-bold" style="background-color: #dc3545;"
+    onmouseover="this.style.backgroundColor='#a71d2a'" 
+    onmouseout="this.style.backgroundColor='#dc3545'" 
+    data-bs-toggle="modal" data-bs-target="#returnModal">
+    <i class="fa-solid fa-circle-plus"></i> Reservar Hotel-Aeropuerto
+</button>
+
+<button type="button" class="btn text-white fw-bold" style="background-color: #28a745;"
+    onmouseover="this.style.backgroundColor='#1e7e34'" 
+    onmouseout="this.style.backgroundColor='#28a745'" 
+    data-bs-toggle="modal" data-bs-target="#roundTripModal">
+    <i class="fa-solid fa-circle-plus"></i> Reservar ida-vuelta (Aeropuerto-Hotel/Hotel-Aeropuerto)
+</button>
+
+        <!--
             <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#oneWayModal"><i class="fa-solid fa-circle-plus"></i> Reservar Aeropuerto-Hotel</button>
             <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#returnModal"><i class="fa-solid fa-circle-plus"></i> Reservar Hotel-Aeropuerto</button>
             <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#roundTripModal"><i class="fa-solid fa-circle-plus"></i> Reservar ida-vuelta (Aeropuerto-Hotel/Hotel-Aeropuerto)</button>
+        -->
         </div>
     </div>
-
+<hr>
     <!-- Tabla reservas ida -->
     <div class="container py-5">
         <h5>Reservas realizadas Aeropuerto-Hotel</h5>
